2911e259ed30f6dbce1e5bdad9aa145f53af8a3b
Darkolas091 <Darkolas091@users.noreply.github.com>
2022-12-22 21:22:51 +0100

    minor tweaks

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,13 @@
-/*
-	Read Only by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
+
 
 (function($) {
+
+	//Scroll
+
+
+
+
+
 
 	var $window = $(window),
 		$body = $('body'),
